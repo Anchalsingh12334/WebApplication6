22223ff3a7a3fd61dd95c7a676b95fbbe0cfb995
dell <dell@DESKTOP-7AQICT6>
2022-02-23 12:37:59 +0530

    Add project files.

---
+++ WebApplication6/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class CustomerController : Controller
+
+    {
+        private ApplicationDbContext dbContext;
+
+        public CustomerController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Location>
+                Locations = dbContext.Locations.ToList();
+            return View(Locations);
+        }
+        public IActionResult CustomerList (int id)
+        {
+            List<Customer>
+                Customers
+                = dbContext.Customers.Where
+                (e => e.Location.Id == id).ToList();
+            return View(Customers);
+        }
+    }
+}
